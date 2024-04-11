2314c8b58462f612fe71831335c76cc73abd21c5
Huy <tuanhuy1104pro@gmail.com>
2024-04-11 21:02:18 +0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Course_Project.Controllers
+{
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+        [Route("[action]")]
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
