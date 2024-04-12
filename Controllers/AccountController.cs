338d01f97826ac8bef01124210049cf2b21412de
Huy <tuanhuy1104pro@gmail.com>
2024-04-12 16:31:15 +0700

    view error fixed

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Course_Project.Controllers
+{
+    [Route("[controller]")]
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        [Route("[action]")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public IActionResult Login(LoginRequestDTO loginrequest)
+        {
+            return View();
+        }
+    }
+}
